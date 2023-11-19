f7b28a698b0adb714d74231d3fc0d668adcbeade
Roy Liu <liuwei811007@gmail.com>
2023-11-19 22:32:26 +0800

    add test api

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,11 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/test', function (Request $request) {
+    $message = $request['message'] ?? "";
+    return "{ 
+        \"test\":\"Hello world\",
+        \"message\":\"$message\"
+    }";
+});
